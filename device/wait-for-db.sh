c290f2a023c59c385b5dff43f8f4e5ca88d487cb
Bouali <youssefbouali206@gmail.com>
2025-10-29 22:18:19 +0100

    Add Device module

--- device/wait-for-db.sh
+++ device/wait-for-db.sh
@@ -4,7 +4,7 @@ host="$1"
 shift
 cmd="$@"
 
-until python -c "import psycopg2; psycopg2.connect(dbname='device_db', user='device_user', password='password', host='$host')" >/dev/null 2>&1; do
+until python -c "import psycopg2; psycopg2.connect(dbname='device_db', user='device_user', password='device_pass', host='$host')" >/dev/null 2>&1; do
   echo "Waiting for Postgres at $host..."
   sleep 2
 done
