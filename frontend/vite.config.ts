4225d7d58b380696282ff85828c2cafef22c7c8c
Bouali <youssefbouali206@gmail.com>
2025-11-09 19:53:26 +0100

    Main Docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+	allowedHosts: "all",
     fs: {
       allow: ["./client", "./shared"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
