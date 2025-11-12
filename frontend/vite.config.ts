bd94d659925cfc8d0834cc8f2ad2aac282f5c498
hamza03-SE <hamza.erradi.64@edu.uiz.ac.ma>
2025-11-12 19:39:00 +0100

    autorise le vite de servir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { defineConfig, Plugin } from "vite";
 import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
 	allowedHosts: ["eduapp.mywire.org", "localhost"],
     fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
+      allow: [
+        "./",
+        "./client",
+        "./shared",
+        "../",         // autorise un niveau au-dessus si besoin
+      ],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**"],
+},
   },
   build: {
     outDir: "dist/spa",
